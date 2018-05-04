f60299e7f53ad701e0af253b4412c3d599ae4d65
刘坤涛 <lkuntao@163.com>
2018-05-04 09:31:43 +0800

    撤销测试代码

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,3 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers:X-Token,Content-Type,Authorization');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
-//Route::group(['domain' => 'admin.agency.com', 'namespace' => 'API'], function () {
-Route::group(['namespace' => 'API'], function () {
-    Route::get('/test',function () {
-        dd(123);
-    });
-
-});
