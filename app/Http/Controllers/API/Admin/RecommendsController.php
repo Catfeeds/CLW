2a2b92b63f1e62e424c08f530882db6e42e3c0ce
lkt <lkuntao@163.com>
2018-05-05 01:21:06 +0000

    精品推荐功能

---
+++ app/Http/Controllers/API/Admin/RecommendsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\API\APIBaseController;
+use App\Models\recommend;
+use Illuminate\Http\Request;
+
+class RecommendsController extends APIBaseController
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(recommend $recommend)
+    {
+        //
+    }
+
+
+    public function edit(recommend $recommend)
+    {
+        //
+    }
+
+
+    public function update(Request $request, recommend $recommend)
+    {
+        //
+    }
+
+
+    public function destroy(recommend $recommend)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/Admin/RecommendsRequest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RecommendsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function messages()
+    {
+        switch ($this->method()) {
+            case 'POST':
+                return [
+
+                ];
+            case 'PUT':
+            case 'PATCH':
+
+            case 'GET':
+            case 'DELETE':
+            default:
+                {
+                    return [];
+                }
+        }
+    }
+
+    /**
+     * 说明: 字段验证
+     *
+     * @return array
+     * @author 刘坤涛
+     */
+    public function rules()
+    {
+        switch ($this->method()) {
+            case 'POST':
+                return [
+                    'name' => 'required|max:32',
+                    'sort' => 'integer',
+                    'shelf' => 'integer',
+                    'show' => 'integer',
+                    'icon' => 'required',
+                    'detail' => 'required|array'
+                ];
+            case 'PUT':
+            case 'PATCH':
+            return [
+                'name' => 'required|max:32',
+                'sort' => 'integer',
+                'shelf' => 'integer',
+                'show' => 'integer',
+                'icon' => 'required',
+                'detail' => 'required|array'
+            ];
+            case 'GET':
+            case 'DELETE':
+            default:
+                {
+                    return [];
+                }
+        }
+    }
+}
