326fd1e598ac0f2e65135cc1d6768d569f59c9a1
王成 <14888748@qq.com>
2018-05-07 02:40:27 +0000

    banner,楼盘推荐,热门楼盘,精品楼盘推荐,服务,系统公告列表

---
+++ app/Http/Controllers/API/App/RecommendsController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers\API\App;
+
+use App\Http\Controllers\API\APIBaseController;
+use App\Repositories\RecommendsRepository;
+
+class RecommendsController extends APIBaseController
+{
+    /**
+     * 说明: 获取精品推荐列表
+     * @param RecommendsRepository $repository
+     * @return \Illuminate\Http\JsonResponse
+     * @author 王成
+     */
+    public function index
+    (
+        RecommendsRepository $repository
+    )
+    {
+        $res = $repository->recommendsList();
+        return $this->sendResponse($res,'精品推荐列表获取成功');
+    }
+}
