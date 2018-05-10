79f105a2b270ed52f4420c158fbd2a4984943667
刘坤涛 <lkuntao@163.com>
2018-05-10 06:21:42 +0000

    首页精品推荐跳转

--- app/Http/Controllers/API/App/RecommendsController.php
+++ app/Http/Controllers/API/App/RecommendsController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\API\App;
 
 use App\Http\Controllers\API\APIBaseController;
+use App\Http\Requests\Admin\RecommendsRequest;
 use App\Repositories\RecommendsRepository;
 
 class RecommendsController extends APIBaseController
@@ -20,5 +21,23 @@ class RecommendsController extends APIBaseController
     {
         $res = $repository->recommendsList();
         return $this->sendResponse($res,'精品推荐列表获取成功');
+    }
+
+    /**
+     * 说明: 点击精品推荐跳转楼盘列表
+     *
+     * @param RecommendsRepository $repository
+     * @param RecommendsRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     * @author 刘坤涛
+     */
+    public function create
+    (
+        RecommendsRepository $repository,
+        RecommendsRequest $request
+    )
+    {
+        $res = $repository->getBuildingList($request);
+        return $this->sendResponse($res, '楼盘列表获取成功');
     }
 }
