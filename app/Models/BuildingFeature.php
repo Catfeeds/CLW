365ac773295e24a08f2bae9ed9ea89046a284bb0
刘坤涛 <lkuntao@163.com>
2018-05-17 09:01:16 +0000

    楼盘特色修改

--- app/Models/BuildingFeature.php
+++ app/Models/BuildingFeature.php
@@ -8,4 +8,20 @@ class BuildingFeature extends Model
 {
     protected $connection = 'mysql';
     protected $guarded = [];
+    protected $appends = ['feature_cn'];
+
+    /**
+     * 说明: 特色图片
+     *
+     * @author 刘坤涛
+     */
+    public function getFeatureCnAttribute()
+    {
+        return collect($this->pic)->map(function($img) {
+            return [
+                'name' => $img,
+                'url'  => config('setting.qiniu_url') . $img
+            ];
+        });
+    }
 }
