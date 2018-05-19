23c400facc36b5c61d33e627b3a027ccec4633dc
jacklin <361525737@qq.com>
2018-05-19 16:21:46 +0800

    修改路由

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,6 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
  */
 Route::group(['namespace' => 'API'], function () {
     include_route_files(__DIR__ . '/API/');
-
 });
 
 
