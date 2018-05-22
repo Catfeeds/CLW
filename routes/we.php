78b1987287ea4320044c08333a3a3733b4128c1b
jacklin <361525737@qq.com>
2018-05-22 11:08:06 +0800

    添加微信页面

--- routes/we.php
+++ routes/we.php
@@ -1,4 +1,9 @@
 <?php
 // we.clw.com
-
+// 首页
 Route::get('/', 'IndexController@index');
+// 楼盘列表页
+Route::resource('buildings', 'BuildingController');
+// 房源详情页
+Route::resource('houses', 'HouseController');
+// 用户相关
