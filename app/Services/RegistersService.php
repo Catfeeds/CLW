140f63dfac7256c6826006cee2f06c26363e0dd5
luozhen <15902773450@163.com>
2018-05-22 14:49:53 +0800

    登录注册快速登录视图

--- app/Services/RegistersService.php
+++ app/Services/RegistersService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Redis\MasterRedis;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class RegistersService
 {
@@ -47,37 +46,4 @@ class RegistersService
 
         return ['status' => true, 'token' => $token];
     }
-
-    public function weAddUser(
-        $request
-    )
-    {
-        $masterRedis = new MasterRedis();
-
-        $key = config('redisKey.STRING_SMSCODE_') . 'register:' . $request->tel;
-        // 验证手机短信是否正确
-        $telCaptcha = $masterRedis->getString($key, 'old');
-        // 判断验证码是否存在
-        if (empty($telCaptcha)) return ['status' => false, 'message' => '验证码失效,请重新发送短息'];
-        // 判断验证码是否正确
-        if ($request->smsCode != $telCaptcha) return ['status' => false, 'message' => '手机验证码错误，请重新输入'];
-
-        // 开始写入表
-        $user = User::create([
-            'tel' => $request->tel,
-            'password' => bcrypt($request->password),
-        ]);
-        if (empty($user)) return ['status' => false, 'message' => '注册失败'];
-
-        // 注册成功，删除验证码
-//        $masterRedis->delKey($key);
-
-
-        Session('a','b');
-
-
-
-
-    }
-    
 }
--- routes/we.php
+++ routes/we.php
@@ -10,6 +10,11 @@ Route::get('/sms/captcha/{tell}/{tmp}', 'RegistersController@getSmsCode');
 // 注册
 Route::resource('registers', 'RegistersController');
 
+// 密码登录
+Route::resource('logins', 'LoginsController');
+// 快速登录页面
+Route::get('quick_login', 'LoginsController@quickLogin');
+
 // 楼盘列表页
 Route::resource('buildings', 'BuildingController');
 // 房源详情页
