091687319b06eda4d7836084fb65ba347723019f
张洵之 <1226580878@qq.com>
2018-05-22 10:06:27 +0800

    项目模版初始化

---
+++ resources/views/we/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//css/mui/mui.css">
+    <title>@section('title')楚楼网--找写字楼上楚楼网十分钟轻松搞定@show</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    @yield('header')
+</head>
+<body>
+@yield('body')
+</body>
+</html>
