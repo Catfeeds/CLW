03f7e7fb297755bfe06275e2fed98a62203714e9
张洵之 <1226580878@qq.com>
2018-05-23 09:19:57 +0800

    修改

--- resources/assets/js/we_building_index.js
+++ resources/assets/js/we_building_index.js
@@ -17,13 +17,14 @@ var app = new Vue({
   },
   methods: {
     changeData: function (data) {
-      this.list = []
-      var params = JSON.parse(JSON.stringify(data))
-      params.area_id = data.area_id === 'all'? null : data.area_id
-      params.block_id = data.block_id === 'all'? null : data.block_id
-      params.renovation = data.renovation === 'all'? null : data.renovation // 装修
-      params.features = data.features === 'all'? null : data.features // 决策偏好
-      this.search = params
+      this.list = [];
+      var params = JSON.parse(JSON.stringify(data));
+      console.log(params)
+      // params.area_id = data.area_id === 'all'? null : data.area_id
+      // params.block_id = data.block_id === 'all'? null : data.block_id
+      // params.renovation = data.renovation === 'all'? null : data.renovation // 装修
+      // params.features = data.features === 'all'? null : data.features // 决策偏好
+      // this.search = params
     }
   }
 });
