28739eb027a8634e54c2ec76e37941dddc22b60d
刘坤涛 <lkuntao@163.com>
2018-05-23 01:32:03 +0000

    楼盘列表数据

--- app/Http/Controllers/We/BuildingController.php
+++ app/Http/Controllers/We/BuildingController.php
@@ -11,6 +11,15 @@ use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
+    /**
+     * 说明: 楼盘列表 试图
+     *
+     * @param BuildingsRepository $buildingsRepository
+     * @param Request $request
+     * @param BuildingsService $service
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author 刘坤涛
+     */
     public function index
     (
         BuildingsRepository $buildingsRepository,
@@ -21,6 +30,27 @@ class BuildingController extends Controller
         $res = $buildingsRepository->buildingList($request, $service);
         return view('we.building_index')->with('data', $res);
     }
+
+    /**
+     * 说明: 楼盘列表数据
+     *
+     * @param BuildingsRepository $buildingsRepository
+     * @param Request $request
+     * @param BuildingsService $service
+     * @return mixed
+     * @author 刘坤涛
+     */
+    public function create
+    (
+        BuildingsRepository $buildingsRepository,
+        Request $request,
+        BuildingsService $service
+    )
+    {
+        $res = $buildingsRepository->buildingList($request, $service);
+        return $res;
+    }
+
 
     // 楼盘详情
     public function show
