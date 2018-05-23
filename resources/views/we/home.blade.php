6b9dee857a594e47eba039fb07c62bf6fcbbb783
张洵之 <1226580878@qq.com>
2018-05-23 10:28:54 +0800

    底部抽离

--- resources/views/we/home.blade.php
+++ resources/views/we/home.blade.php
@@ -184,34 +184,8 @@
             </div>
         </div>
         <!-- 底部导航 -->
-        <div id="alltab" style="display:flex;position:fixed;justify-content:space-around;background:white;height:49px;z-index:300;bottom:0;
-        width:100%;padding:5px 0;border-top:1px solid #f4f4f4;">
-            <div class="tabs" style="text-align:center">
-                <img src="/we_img/tab1.png" style="display:none;height:22px;margin-bottom:-4px;">
-                <img src="/we_img/tab1_active.png" style="height:22px;margin-bottom:-4px;">
-                <div style="font-size:11px;" class="active">首页</div>
-            </div>
-            <div class="tabs" style="text-align:center">
-                <img src="/we_img/tab2.png" style="height:22px;margin-bottom:-4px;">
-                <img src="/we_img/tab2_active.png" style="display:none;height:22px;margin-bottom:-4px;">
-                <div style="font-size:11px;">找房</div>
-            </div>
-            <div class="tabs" style="text-align:center">
-                <img src="/we_img/tab3.png" style="height:22px;margin-bottom:-4px;">
-                <img src="/we_img/tab3_active.png" style="display:none;height:22px;margin-bottom:-4px;">
-                <div style="font-size:11px;">服务</div>
-            </div>
-            <div class="tabs" style="text-align:center">
-                <img src="/we_img/tab4.png" style="height:22px;margin-bottom:-4px;">
-                <img src="/we_img/tab4_active.png" style="display:none;height:22px;margin-bottom:-4px;">
-                <div style="font-size:11px;">我的</div>
-            </div>
-        </div>
+        @include('we.tab')
     </div>
     <script src="/js/we_home.js"></script>
-    <style>
-        .active{
-            color:#007AFF;
-        }
-    </style>
+
 @endsection
