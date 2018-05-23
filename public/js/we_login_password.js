426f63056ff09930f50e68b4b021fed4f3b92197
wanghao <491993108@qq.com>
2018-05-23 16:16:49 +0800

    完成忘记密码

--- public/js/we_login_password.js
+++ public/js/we_login_password.js
@@ -10451,10 +10451,96 @@ module.exports = __webpack_require__(85);
 window.$ = window.jQuery = __webpack_require__(0);
 var tel = $('#tel'),
     smsCode = $('#sms'),
-    getSms = $('#getSms');
-/**
- * Created by zxz1992 on 2018/5/22.
- */
+    getSms = $('#getSms'),
+    password = $('#password');
+
+$(document).on('touchend || tap', '#getSms', function (e) {
+    if (!tel.val() || tel.val().trim() === '') {
+        alert('请输入手机号码');
+        return false;
+    }
+    var pathStr = tel.val() + '/' + 'register';
+    $.ajax({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: '/sms/captcha/' + pathStr,
+        type: 'get',
+        success: function success(res) {
+            if (res.data) {
+                getSms.html(120 + 's');
+                var time = setInterval(function () {
+                    getSms.html(parseInt(getSms.html()) - 1 + 's');
+                    if (!parseInt(getSms.html())) {
+                        getSms.html('获取验证码');
+                        window.clearInterval(time);
+                    }
+                }, 1000);
+                alert('短信发送成功');
+            }
+        },
+        error: function error(res) {
+            alert(responseJSON.message);
+        }
+    });
+});
+
+$(document).on('touchend || tap', '.loginBtn button', function (e) {
+    var tel_num = tel.val(),
+        smsCode_num = smsCode.val(),
+        password_num = password.val();
+
+    if (!tel_num || tel_num.trim() === '') {
+        alert('请输入手机号码');
+        return false;
+    }
+    if (!smsCode_num || smsCode_num.trim() === '') {
+        alert('请输入验证码');
+        return false;
+    }
+    if (!password_num || password_num.trim() === '') {
+        TopTips({
+            message: '请输入密码',
+            duration: 3000
+        });
+        return false;
+    }
+    if (password_num.length < 6) {
+        TopTips({
+            message: '密码最小长度为6',
+            duration: 3000
+        });
+        return false;
+    }
+    if (password_num.length > 18) {
+        TopTips({
+            message: '密码最大长度为18',
+            duration: 3000
+        });
+        return false;
+    }
+    $.ajax({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: "/retrieve_pwd",
+        type: 'post',
+        data: {
+            tel: tel_num,
+            smsCode: smsCode_num,
+            password: password_num
+        },
+        success: function success(data) {
+            alert(data.message);
+            if (data.success) {
+                window.location.href = '/user';
+            }
+        },
+        error: function error(data) {
+            alert(data.responseJSON.message);
+        }
+    });
+});
 
 /***/ })
 
