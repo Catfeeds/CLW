9b08e4c42357349426c8b3e2c691b752967b5d82
wanghao <491993108@qq.com>
2018-05-24 09:26:40 +0800

    修改服务页样式

--- public/js/we_user_revise_phone2.js
+++ public/js/we_user_revise_phone2.js
@@ -1,1 +1,22 @@
-webpackJsonp([5],{113:function(n,o,t){n.exports=t(114)},114:function(n,o,t){window.$=window.jQuery=t(0),$(document).on("touchend || tap",".loginOut",function(){console.log("修改成功")})}},[113]);
+webpackJsonp([5],{
+
+/***/ 115:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(116);
+
+
+/***/ }),
+
+/***/ 116:
+/***/ (function(module, exports, __webpack_require__) {
+
+window.$ = window.jQuery = __webpack_require__(0);
+// 修改电话号码
+$(document).on('touchend || tap', '.loginOut', function () {
+    console.log('修改成功');
+});
+
+/***/ })
+
+},[115]);
