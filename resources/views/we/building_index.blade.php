abf24fe2464f486a722ed546954412a491c61626
wanghao <491993108@qq.com>
2018-05-25 14:20:56 +0800

    删除找房中写字楼字样

--- resources/views/we/building_index.blade.php
+++ resources/views/we/building_index.blade.php
@@ -7,12 +7,6 @@
     <input id="pageOne" value="{{json_encode($data)}}" type="hidden"/>
     <input id="request" value="{{json_encode($req)}}" type="hidden">
     <div id="buildingList">
-        <header id="header" class="mui-bar mui-bar-nav">
-            <h1 class="mui-title">写字楼</h1>
-            <div class="select-box">
-                <building-select ref='buildingSelect' @@flush-data='changeData'></building-select>
-            </div>
-        </header>
         <div class="main-content">
             <building-list position-img='/we_img/index_positon.png' good-img='/we_img/index_good.png' :list='list'></building-list>
             <div class="more" v-if="getData" @touchend='getMore'>
