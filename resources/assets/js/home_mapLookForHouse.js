32e740019a2abf0a5de51301d1509ba45912759f
张洵之 <1226580878@qq.com>
2018-06-05 19:12:01 +0800

    修改脚手架

--- resources/assets/js/home_mapLookForHouse.js
+++ resources/assets/js/home_mapLookForHouse.js
@@ -2,3 +2,11 @@
  * Created by zxz1992 on 2018/6/5.
  */
 require('./home_common');
+import BaiduMap from 'vue-baidu-map'
+Vue.use(BaiduMap, {
+  ak: process.env.baiduAK
+});
+
+new Vue({
+  el: '#app'
+});
--- resources/views/test/mapDemo.blade.php
+++ resources/views/test/mapDemo.blade.php
@@ -5,9 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{res('/css/home_mapLookForHouse.css')}}">
     <title>地图demo</title>
 </head>
 <body>
-123
+<div id="app">
+    <baidu-map class="map" style="display: flex;flex:1;flex-direction: column" center="北京">
+        <bm-view style="width: 100%; flex: 1"></bm-view>
+    </baidu-map>
+    <script src="{{res('/js/home_manifest.js')}}"></script>
+    <script src="{{res('/js/home_vendor.js')}}"></script>
+    <script src="{{res('/js/home_mapLookForHouse.js')}}"></script>
+</div>
 </body>
 </html>
