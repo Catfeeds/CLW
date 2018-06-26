0f0a8367eb8640c849be5de3d128292a7ad9e384
刘坤涛 <lkuntao@163.com>
2018-06-26 09:06:42 +0000

    pc端房源详情,相关房源

--- routes/home.php
+++ routes/home.php
@@ -8,3 +8,5 @@ Route::resource('buildings', 'BuildingsController');
 
 //房源详情
 Route::resource('office_building_houses', 'OfficeBuildingHousesController');
+// 房源下的相关房源
+Route::get('rim_houses/{id}','OfficeBuildingHousesController@showOffice');
