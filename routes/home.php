872bd919b3b18afd5325a52a388948b51b096530
刘坤涛 <lkuntao@163.com>
2018-06-26 07:25:49 +0000

    Pc房源详情

--- routes/home.php
+++ routes/home.php
@@ -3,6 +3,8 @@
 // 前台首页
 Route::resource('/', 'IndexController');
 
-
 // 楼盘
 Route::resource('buildings', 'BuildingsController');
+
+//房源详情
+Route::resource('house', 'OfficeBuildingHousesController');
