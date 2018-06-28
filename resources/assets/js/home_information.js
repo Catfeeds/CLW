cc102e96323a867f8fa224746cb4872eee618e36
wanghao <491993108@qq.com>
2018-06-28 18:50:37 +0800

    完成资讯图片和新闻轮播切换

--- resources/assets/js/home_information.js
+++ resources/assets/js/home_information.js
@@ -1,7 +1,18 @@
 require('./home_common');
 const Swiper = require('swiper');
 
-var mySwiper = new Swiper('.swiper-container',{
+var mySwiper1 = new Swiper('.swiper-container',{
     pagination : '.swiper-pagination',
-    width: 770
+    effect : 'fade',
+    width: 770,
+    onTransitionEnd:function (swiper) {
+        console.log(swiper.activeIndex);
+        mySwiper2.slideTo(swiper.activeIndex);
+    }
+})
+var mySwiper2 = new Swiper('.swiper-container1',{
+    effect : 'fade',
+    fade: {
+        crossFade: true,
+    }
 })
