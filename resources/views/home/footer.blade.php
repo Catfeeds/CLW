a55f82eb9df9b5512889a1e39aeffab71c0f8f47
wujie <790715323@qq.com>
2018-07-10 16:00:56 +0800

    合并

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -36,13 +36,13 @@
         <div class="code_download f_r">
             <div class="code_title">扫码下载</div>
             <div class="code_area clearfix">
-                <div class="wx_code f_l" style="margin-right:53px">
-                    <img src="/home_img/footer_code.png" alt="">
-                    <div>扫码关注微信公众号</div>
+                <div class="wx_code f_l" style="margin-right:25px">
+                    <img src="/home_img/foot_code.png" alt="">
+                    <div>扫码关注公众号</div>
                 </div>
                 <div class="wx_code f_r">
-                    <img src="/home_img/footer_code.png" alt="">
-                    <div>扫码关注微信公众号</div>
+                    <img src="/home_img/foot_code.png" alt="">
+                    <div>APP下载</div>
                 </div>
             </div>
         </div>
