a88018e5a8e232329c3fe177c75552c09eff0825
刘坤涛 <lkuntao@163.com>
2018-07-11 07:58:48 +0000

    商城标签管理

--- routes/API/admin.php
+++ routes/API/admin.php
@@ -261,6 +261,15 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
 
         //pc端企业服务
         Route::resource('pc_enterprise_services', 'PcEnterpriseServicesController');
+
+        /*
+        |--------------------------------------------------------------------------
+        | 商城标签管理
+        |--------------------------------------------------------------------------
+        */
+
+        Route::resource('labels', 'LabelsController');
+
     });
 
 });
