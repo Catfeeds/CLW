4c0b305b255f3a6a68d6adbd2eb4c72b3b51e2a9
luozhen <15902773450@163.com>
2018-07-11 14:39:48 +0800

    楼盘列表价格排序

---
+++ database/migrations/2018_07_11_104046_create_labels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('category_id')->nullable()->comment('类id');
+            $table->tinyInteger('parent_id')->nullable()->comment('父级id');
+            $table->string('name','32')->nullable()->comment('标签名');
+            $table->tinyInteger('stage')->nullable()->comment('等级');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        DB::statement("alter table `labels` comment'商城标签表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+}
