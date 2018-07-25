97ab9e0248417811a09f9a132a007870162a1210
zhengyu <1130367447@qq.com>
2018-07-25 10:08:43 +0000

    修改转化率

--- app/Services/StatisticsService.php
+++ app/Services/StatisticsService.php
@@ -160,16 +160,18 @@ class StatisticsService
     {
         $date = $this->getDateByParam($request->time);
         //渠道总数量
-
+        $data = [];
         $count1 = EntrustThrowIn::whereIn('type',[1,2])->whereBetween('created_at',$date)->count();
         $count2 = RawCustom::whereIn('source',[1,2])->whereBetween('created_at',$date)->count();
         $count = $count1 + $count2;
+        $data['count_x'] = $count;
         //工单列表中有效状态中状态为有效的数量
         $count3 = RawCustom::where('valid',1)->whereBetween('created_at',$date)->count();
+        $data['count_y'] = $count3;
         //工单列表中是否成交状态的成交的数量
         $count4 = RawCustom::where('clinch',1)->whereBetween('created_at',$date)->count();
+        $data['count_z'] = $count4;
         //渠道有效转化率
-        $data = [];
         $data['ecroc']= round($count3 / $count ,3) *100 . '%';
         //成交转化率
         $data['turnover_rate'] = round($count4 /$count3 ,3) *100 .'%';
