2c02135f168d3509949e15dd3ca497b2e9c22d23
张洵之 <1226580878@qq.com>
2018-08-07 18:11:43 +0800

    侧边栏修改

--- resources/assets/js/components/home/right_tab/listData.vue
+++ resources/assets/js/components/home/right_tab/listData.vue
@@ -68,7 +68,7 @@ export default {
     .r-panel-txt {
       float: left;
       padding-left: 10px;
-      width: 139px;
+      width: 129px;
       color: #333;
       div{
         font-size: 12px;
