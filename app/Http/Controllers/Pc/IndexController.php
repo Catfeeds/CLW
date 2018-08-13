56cad9e36a55872b4a2bdb1a42408b9d5a189685
luozhen <15902773450@163.com>
2018-08-13 14:04:28 +0800

    搜索框加入删除英文

--- app/Http/Controllers/Pc/IndexController.php
+++ app/Http/Controllers/Pc/IndexController.php
@@ -62,21 +62,10 @@ class IndexController extends Controller
         Request $request
     )
     {
-        // 中文
         $string = "'". $request['selectInfo'] . "'";
-        $res = $this->getSelectRes($string);
-
-        // 英文
-        $pyString = $request['pySelectInfo'];
-
-
-        $regex = "/\/|\～|\，|\。|\！|\？|\“|\”|\【|\】|\『|\』|\：|\；|\《|\》|\’|\‘|\ |\·|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\,|\.|\/|\;|\'|\`|\-|\=|\\\|\|/";
-        $pyString = preg_replace($regex,"",$pyString);
-        $pyString = "'". str_replace("'","", $pyString) . "'";
-        $pyRes = $this->getSelectRes($pyString);
-
-        // 获取所有楼盘id(合并中英文搜索结果合并去重)
-        $buildingIds = array_unique(array_merge($res, $pyRes));
+        $res = \DB::select("select building_id from media.building_keywords where MATCH(keywords) AGAINST($string IN BOOLEAN MODE)");
+        // 获取所有楼盘id
+        $buildingIds = array_column(Common::objectToArray($res), 'building_id');
 
         $res = Building::whereIn('id', $buildingIds)->pluck('name')->toArray();
 
@@ -85,16 +74,5 @@ class IndexController extends Controller
                 'value' => $v
             ];
         }),'通过关键字获取楼盘名成功');
-    }
-
-    // 通过关键字搜索
-    public function getSelectRes($condition)
-    {
-        $res = \DB::select("select building_id from media.building_keywords where MATCH(keywords) AGAINST($condition IN BOOLEAN MODE)");
-
-        if (empty($res)) return array();
-
-        // 获取所有楼盘id
-        return array_column(Common::objectToArray($res), 'building_id');
     }
 }
