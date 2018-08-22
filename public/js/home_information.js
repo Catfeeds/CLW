898284769cd0a7f1fbe6e6bcaae15384c93ece5c
wanghao <491993108@qq.com>
2018-08-22 15:02:15 +0800

    修改咨询中心轮播图点击切换事件

--- public/js/home_information.js
+++ public/js/home_information.js
@@ -7862,6 +7862,7 @@ var mySwiper1 = new Swiper('.swiper-container', {
     width: 770,
     autoplay: 5000, //可选选项，自动滑动
     autoplayDisableOnInteraction: false,
+    paginationClickable: true,
     onTransitionEnd: function onTransitionEnd(swiper) {
         console.log(swiper.activeIndex);
         mySwiper2.slideTo(swiper.activeIndex);
